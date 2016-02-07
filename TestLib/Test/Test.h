5d436b560d09e99916778500230fb39357852d30
JustinYang <ityangjing@gmail.com>
2016-02-07 18:40:01 +0800

    new

---
+++ TestLib/Test/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  TestLib
+//
+//  Created by JustinYang on 2/7/16.
+//  Copyright © 2016 JustinYang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ TestLib/Test/Test.m
@@ -0,0 +1,15 @@
+//
+//  Test.m
+//  TestLib
+//
+//  Created by JustinYang on 2/7/16.
+//  Copyright © 2016 JustinYang. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+-(void)libTest{
+    NSLog(@"%@",[self class]);
+}
+@end
